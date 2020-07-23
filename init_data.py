a960eb0eaa28100caba89f07372aec3d7687644f
Dássone J <dyotamo@gmail.com>
2020-07-23 12:38:38 +0200

    Seed.

--- init_data.py
+++ init_data.py
@@ -1,30 +1,7 @@
-from bs4 import BeautifulSoup
 from werkzeug.security import generate_password_hash
 
 from app import app, db
-from models import Brew, User
-
-
-class BrewModel:
-    def __init__(self, name, unit_price):
-        self.name = name
-        self.unit_price = unit_price
-
-    def __repr__(self):
-        return self.name
-
-
-def parse():
-    brews = []
-    with open('fixture/brew.html') as fixture:
-        soup_obj = BeautifulSoup(fixture, 'html.parser')
-        for brew in soup_obj.find_all('brew'):
-            brews.append(
-                BrewModel(
-                    name=brew.text.strip(),
-                    unit_price=float(brew['unit_price']),
-                ))
-        return brews
+from models import User
 
 
 def _generate_user():
@@ -35,16 +12,6 @@ def _generate_user():
         ))
 
 
-def _generate_brews(brews):
-    for prod in brews:
-        db.session.add(Brew(
-            name=prod.name,
-            unit_price=prod.unit_price,
-        ))
-
-
 if __name__ == '__main__':
     with app.app_context():
-        _generate_user()
-        _generate_brews(parse())
         db.session.commit()
